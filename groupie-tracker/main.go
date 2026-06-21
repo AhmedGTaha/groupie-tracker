70de22af32531c55c0f59f0dfd38b1bfcdc0b5ea
AhmedGTaha <ahmedtahaghodhbani@gmail.com>
2026-06-22 02:23:14 +0300

    added artist handler and added home html file.

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"       // write text responses
-	"log"       // print server messages
-	"net/http"  // tools to build the server
+	"fmt"           // write text responses
+	"log"           // print server messages
+	"net/http"      // tools to build the server
+	"html/template" // load and render HTML templates
 )
-
 
 // A handler is a function that runs when the browser visits a route
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// w http.ResponseWriter is used to send a response back to the browser
 	// r *http.Request contains information about the request, like the URL path
-	
+
 	// This checks if the user visited a wrong path
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -22,15 +22,32 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Server is running")
 }
 
+// It works the same way as homeHandler, but it is responsible for the /artists page
+func artistsHandler(w http.ResponseWriter, r *http.Request) {
+	// w http.ResponseWriter is used to send a response back to the browser
+	// r *http.Request contains information about the request, like the URL path
+
+	// This checks if the user visited a wrong path
+	if r.URL.Path != "/artists" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// This sends text back to the browser
+	fmt.Fprint(w, "Artists page")
+}
+
 func main() {
 	// mux = multiplexer
 	// A mux is a router it decides which function should handle each route
 	mux := http.NewServeMux()
 
 	// This allows the server to serve files from the static folder
+	// This registers a new route. Any URL that starts with "/static/"
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/artists", artistsHandler)
 
 	log.Println("Server started at http://localhost:8080")
 
