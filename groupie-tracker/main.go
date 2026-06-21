a384fc00ecfe9d41d493d91200843de19058570d
AhmedGTaha <ahmedtahaghodhbani@gmail.com>
2026-06-22 02:02:47 +0300

    built the we server

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"fmt"       // write text responses
+	"log"       // print server messages
+	"net/http"  // tools to build the server
 )
 
+
+// A handler is a function that runs when the browser visits a route
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// w http.ResponseWriter is used to send a response back to the browser
+	// r *http.Request contains information about the request, like the URL path
+	
+	// This checks if the user visited a wrong path
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// This sends text back to the browser
+	fmt.Fprint(w, "Server is running")
+}
+
 func main() {
-	fmt.Print("Hello Ahmed")
+	// mux = multiplexer
+	// A mux is a router it decides which function should handle each route
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", homeHandler)
+
+	log.Println("Server started at http://localhost:8080")
+
+	// err is an error var used to store errors returned by http funcs
+	// This starts the server on port 8080
+	err := http.ListenAndServe(":8080", mux)
+
+	// If the server cannot start, print error
+	if err != nil {
+		log.Fatal(err)
+	}
 }
